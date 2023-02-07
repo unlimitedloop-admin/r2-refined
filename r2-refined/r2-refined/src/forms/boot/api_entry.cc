f19d9fb188bb074ae7d5d000e3ff3c17ec577f87
unlimitedloop-admin <atomic-u7.effixy@loopunlimited-rootone.com>
2023-02-07 22:36:09 +0900

    VC++プロジェクトのセットアップ、editorconfigの作成。

---
+++ r2-refined/r2-refined/src/forms/boot/api_entry.cc
@@ -0,0 +1,58 @@
+/**************************************************************/
+//
+//
+//      Copyright (c) 2023 UNLIMITED LOOP ROOT-ONE
+//
+//
+//      This software(and source code) is completely Unlicense.
+//      see "LICENSE.md".
+//
+//
+/**************************************************************/
+//
+//
+//      r2-refined project
+//
+//      File name       : api_entry.cc
+//
+//      Author          : u7
+//
+//      Last update*    : 
+//
+//      File version*   : 
+//
+//
+/**************************************************************/
+
+/*
+ * =============================================================
+ *  FILE DESCRIPTION
+ * =============================================================
+ * <Enter a description of some file here.>
+**/
+
+
+
+/* INCLUDES */
+// GENERAL USING HEADER
+#include <Windows.h>
+
+
+
+/* SOURCES */
+/// <summary>
+/// The entry point of windows application.
+/// </summary>
+/// <param name="hInstance">Handle to an instance</param>
+/// <param name="hPrevInstance">Undefined argument, always 0</param>
+/// <param name="lpszCmdLine">Unicode string command line arguments</param>
+/// <param name="nCmdShow">A flag that says whether the main application window will be minimized, maximized, or shown normally</param>
+/// <returns>Argument values for exit</returns>
+INT APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpszCmdLine, _In_ int nCmdShow) {
+
+
+
+
+    return EXIT_SUCCESS;
+
+}
