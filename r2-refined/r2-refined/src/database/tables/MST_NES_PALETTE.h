2403d0c3419bed2bb404b215f80fe10fe9b858c4
unlimitedloop-admin <atomic-u7.effixy@loopunlimited-rootone.com>
2023-03-06 21:04:09 +0900

    NES用デフォルトパレットデータを作成。

--- r2-refined/r2-refined/src/database/tables/MST_NES_PALETTE.h
+++ r2-refined/r2-refined/src/database/tables/MST_NES_PALETTE.h
@@ -17,9 +17,9 @@
 //
 //      Author          : u7
 //
-//      Last update     : 2023/03/05
+//      Last update     : 2023/03/06
 //
-//      File version    : 1
+//      File version    : 2
 //
 //
 /**************************************************************/
@@ -41,7 +41,6 @@ namespace DB {
 
     namespace MST_NES_PALETTE {
 
-        // TODO : Add default NES palette. 
         void tr_0x00(void);
         void tr_0x01(void);
         void tr_0x02(void);
@@ -64,9 +63,48 @@ namespace DB {
         void tr_0x13(void);
         void tr_0x14(void);
         void tr_0x15(void);
+        void tr_0x16(void);
+        void tr_0x17(void);
+        void tr_0x18(void);
+        void tr_0x19(void);
+        void tr_0x1A(void);
+        void tr_0x1B(void);
+        void tr_0x1C(void);
+        void tr_0x1D(void);
         void tr_0x1E(void);
         void tr_0x1F(void);
         void tr_0x20(void);
+        void tr_0x21(void);
+        void tr_0x22(void);
+        void tr_0x23(void);
+        void tr_0x24(void);
+        void tr_0x25(void);
+        void tr_0x26(void);
+        void tr_0x27(void);
+        void tr_0x28(void);
+        void tr_0x29(void);
+        void tr_0x2A(void);
+        void tr_0x2B(void);
+        void tr_0x2C(void);
+        void tr_0x2D(void);
+        void tr_0x2E(void);
+        void tr_0x2F(void);
+        void tr_0x30(void);
+        void tr_0x31(void);
+        void tr_0x32(void);
+        void tr_0x33(void);
+        void tr_0x34(void);
+        void tr_0x35(void);
+        void tr_0x36(void);
+        void tr_0x37(void);
+        void tr_0x38(void);
+        void tr_0x39(void);
+        void tr_0x3A(void);
+        void tr_0x3B(void);
+        void tr_0x3C(void);
+        void tr_0x3D(void);
+        void tr_0x3E(void);
+        void tr_0x3F(void);
 
     }  // namespace MST_NES_PALETTE
 
--- r2-refined/r2-refined/src/database/tables/mst_nes_palette.cc
+++ r2-refined/r2-refined/src/database/tables/mst_nes_palette.cc
@@ -17,9 +17,9 @@
 //
 //      Author          : u7
 //
-//      Last update     : 2023/03/05
+//      Last update     : 2023/03/06
 //
-//      File version    : 1
+//      File version    : 2
 //
 //
 /**************************************************************/
@@ -88,11 +88,89 @@ namespace DB {
 
         void tr_0x15(void) { SetBackgroundColor(0xE7, 0x00, 0x5B); }
 
+        void tr_0x16(void) { SetBackgroundColor(0xDB, 0x2B, 0x00); }
+
+        void tr_0x17(void) { SetBackgroundColor(0xCB, 0x4F, 0x0F); }
+
+        void tr_0x18(void) { SetBackgroundColor(0x8B, 0x73, 0x00); }
+
+        void tr_0x19(void) { SetBackgroundColor(0x00, 0x97, 0x00); }
+
+        void tr_0x1A(void) { SetBackgroundColor(0x00, 0xAB, 0x00); }
+
+        void tr_0x1B(void) { SetBackgroundColor(0x00, 0x93, 0x3B); }
+
+        void tr_0x1C(void) { SetBackgroundColor(0x00, 0x83, 0x8B); }
+
+        void tr_0x1D(void) { SetBackgroundColor(0x00, 0x00, 0x00); }
+
         void tr_0x1E(void) { SetBackgroundColor(0x00, 0x00, 0x00); }
 
         void tr_0x1F(void) { SetBackgroundColor(0x00, 0x00, 0x00); }
 
         void tr_0x20(void) { SetBackgroundColor(0xFF, 0xFF, 0xFF); }
+
+        void tr_0x21(void) { SetBackgroundColor(0x3F, 0xBF, 0xFF); }
+
+        void tr_0x22(void) { SetBackgroundColor(0x5F, 0x73, 0xFF); }
+
+        void tr_0x23(void) { SetBackgroundColor(0xA7, 0x8B, 0xFD); }
+
+        void tr_0x24(void) { SetBackgroundColor(0xF7, 0x7B, 0xFF); }
+
+        void tr_0x25(void) { SetBackgroundColor(0xFF, 0x77, 0xB7); }
+
+        void tr_0x26(void) { SetBackgroundColor(0xFF, 0x77, 0x63); }
+
+        void tr_0x27(void) { SetBackgroundColor(0xFF, 0x9B, 0x3B); }
+
+        void tr_0x28(void) { SetBackgroundColor(0xF3, 0xBF, 0x3F); }
+
+        void tr_0x29(void) { SetBackgroundColor(0x83, 0xD3, 0x13); }
+
+        void tr_0x2A(void) { SetBackgroundColor(0x4F, 0xDF, 0x4B); }
+
+        void tr_0x2B(void) { SetBackgroundColor(0x58, 0xF8, 0x98); }
+
+        void tr_0x2C(void) { SetBackgroundColor(0x00, 0xEB, 0xDB); }
+
+        void tr_0x2D(void) { SetBackgroundColor(0x75, 0x75, 0x75); }
+
+        void tr_0x2E(void) { SetBackgroundColor(0x00, 0x00, 0x00); }
+
+        void tr_0x2F(void) { SetBackgroundColor(0x00, 0x00, 0x00); }
+
+        void tr_0x30(void) { SetBackgroundColor(0xFF, 0xFF, 0xFF); }
+
+        void tr_0x31(void) { SetBackgroundColor(0xAB, 0xE7, 0xFF); }
+
+        void tr_0x32(void) { SetBackgroundColor(0xC7, 0xD7, 0xFF); }
+
+        void tr_0x33(void) { SetBackgroundColor(0xD7, 0xCB, 0xFF); }
+
+        void tr_0x34(void) { SetBackgroundColor(0xFF, 0xC7, 0xFF); }
+
+        void tr_0x35(void) { SetBackgroundColor(0xFF, 0xC7, 0xDB); }
+
+        void tr_0x36(void) { SetBackgroundColor(0xFF, 0xBF, 0xB3); }
+
+        void tr_0x37(void) { SetBackgroundColor(0xFF, 0xDB, 0xAB); }
+
+        void tr_0x38(void) { SetBackgroundColor(0xFF, 0xE7, 0xA3); }
+
+        void tr_0x39(void) { SetBackgroundColor(0xE3, 0xFF, 0xA3); }
+
+        void tr_0x3A(void) { SetBackgroundColor(0xAB, 0xF3, 0xBF); }
+
+        void tr_0x3B(void) { SetBackgroundColor(0xB3, 0xFF, 0xCF); }
+
+        void tr_0x3C(void) { SetBackgroundColor(0x9F, 0xFF, 0xF3); }
+
+        void tr_0x3D(void) { SetBackgroundColor(0xBC, 0xBC, 0xBC); }
+
+        void tr_0x3E(void) { SetBackgroundColor(0x00, 0x00, 0x00); }
+
+        void tr_0x3F(void) { SetBackgroundColor(0x00, 0x00, 0x00); }
 
     }  // namespace MST_NES_PALETTE
 
